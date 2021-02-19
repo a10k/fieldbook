de6057eea46894c7fe95e0d7b3cfa3a6d03835ec
a10k <yo@wishyoulization.com>
2021-02-20 04:56:59 +0530

    ctrl+1 to toggle view

--- editor/index.js
+++ editor/index.js
@@ -1,6 +1,5 @@
 import { Runtime, Inspector, Library } from "./runtime.js";
-import interact from 
-'https://cdn.interactjs.io/v1.10.3/interactjs/index.js';
+import interact from "https://cdn.interactjs.io/v1.10.3/interactjs/index.js";
 
 //use fieldbook import for ui
 //import ui from "./52b2eb85b6b641b0@286.js";
@@ -27,8 +26,8 @@ const ui_module = new Runtime().module(ui, (name) => {
 
 const cache = {};
 const config = {};
-
 const root = document.getElementById("fieldbook-root");
+let eye_toggle = true;
 
 //custom files resolver
 const Files = () => {
@@ -54,7 +53,7 @@ const rebuildUi = (d) => {
   config.settings.map((c, i) => {
     let item = cache[c.handle];
     if (item) {
-      item.container.style.order = i;
+      item.container.style.zIndex = 1000000 - i;
       item.container.style.display = c.hide ? "none" : "inline-block";
     }
   });
@@ -99,72 +98,76 @@ const observer_resolver = (handle) => {
       container.appendChild(label);
       observer = Inspector.into(container);
       container.setAttribute("class", handle);
-      container.style.order = settings_obj.order; // can be set by user in ui
+      container.style.zIndex = 1000000 - settings_obj.order; // can be set by user in ui
       container.style.display = settings_obj.hide ? "none" : "inline-block"; // can be set by user in ui
       root.appendChild(container);
+
+      let interact_instance = interact(container)
+        .resizable({
+          // resize from all edges and corners
+          edges: { left: true, right: true, bottom: true, top: true },
+
+          listeners: {
+            move(event) {
+              var target = event.target;
+              var x = parseFloat(target.getAttribute("data-x")) || 0;
+              var y = parseFloat(target.getAttribute("data-y")) || 0;
+
+              // update the element's style
+              target.style.width = event.rect.width + "px";
+              target.style.height = event.rect.height + "px";
+
+              // translate when resizing from top or left edges
+              x += event.deltaRect.left;
+              y += event.deltaRect.top;
+
+              target.style.webkitTransform = target.style.transform =
+                "translate(" + x + "px," + y + "px)";
+
+              target.setAttribute("data-x", x);
+              target.setAttribute("data-y", y);
+              //target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+            },
+          },
+          modifiers: [
+            // keep the edges inside the parent
+            interact.modifiers.restrictEdges({
+              outer: "parent",
+            }),
+
+            // minimum size
+            interact.modifiers.restrictSize({
+              min: { width: 50, height: 50 },
+            }),
+          ],
+
+          inertia: true,
+        })
+        .draggable({
+          listeners: {
+            move(event) {
+              var target = event.target;
+              // keep the dragged position in the data-x/data-y attributes
+              var x =
+                (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+              var y =
+                (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+              // translate the element
+              target.style.webkitTransform = target.style.transform =
+                "translate(" + x + "px, " + y + "px)";
+
+              // update the posiion attributes
+              target.setAttribute("data-x", x);
+              target.setAttribute("data-y", y);
+            },
+          },
+        });
       cache[handle] = {
         observer,
         container,
+        interact_instance,
       };
-      interact(container).resizable({
-        // resize from all edges and corners
-        edges: { left: true, right: true, bottom: true, top: true },
-    
-        listeners: {
-          move (event) {
-            var target = event.target
-            var x = (parseFloat(target.getAttribute('data-x')) || 0)
-            var y = (parseFloat(target.getAttribute('data-y')) || 0)
-    
-            // update the element's style
-            target.style.width = event.rect.width + 'px'
-            target.style.height = event.rect.height + 'px'
-    
-            // translate when resizing from top or left edges
-            x += event.deltaRect.left
-            y += event.deltaRect.top
-    
-            target.style.webkitTransform = target.style.transform =
-              'translate(' + x + 'px,' + y + 'px)'
-    
-            target.setAttribute('data-x', x)
-            target.setAttribute('data-y', y)
-            //target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
-          }
-        },
-        modifiers: [
-          // keep the edges inside the parent
-          interact.modifiers.restrictEdges({
-            outer: 'parent'
-          }),
-    
-          // minimum size
-          interact.modifiers.restrictSize({
-            min: { width: 100, height: 50 }
-          })
-        ],
-    
-        inertia: true
-      })
-      .draggable({
-        listeners: {
-          move (event) {
-            var target = event.target
-            // keep the dragged position in the data-x/data-y attributes
-            var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-            var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-          
-            // translate the element
-            target.style.webkitTransform =
-              target.style.transform =
-                'translate(' + x + 'px, ' + y + 'px)'
-          
-            // update the posiion attributes
-            target.setAttribute('data-x', x)
-            target.setAttribute('data-y', y)
-          }
-        }
-      })
     }
 
     return observer(name);
@@ -255,4 +258,19 @@ socket.on("event", handler);
 socket.on("settings", (data) => {
   config.settings = data || [];
   socket.emit("ready");
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && event.key === "1") {
+    eye_toggle = !eye_toggle;
+    document
+      .querySelector("#fieldbook-content")
+      .setAttribute("class", eye_toggle ? "" : "close_eyes");
+    Object.values(debug.cache).map((d) =>
+      d.interact_instance.draggable({
+        enabled: eye_toggle,
+      })
+    );
+    event.preventDefault();
+  }
 });
